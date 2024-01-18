123d0a6461bef2e6ae65117d031d610af992c129
Konstantin Davydov <davydovks@mail.ru>
2024-01-18 11:04:16 +0500

    move common elements to quiz template

--- resources/views/layouts/quiz.blade.php
+++ resources/views/layouts/quiz.blade.php
@@ -8,7 +8,16 @@
         И мы сделаем <b>визуализацию с индивидуальным подбором</b> изображений<br>по вашему запросу
     </p>
     <div class="quiz__container">
-        @yield('quiz_container')
+        <div class="progress" role="progressbar" aria-label="Progress bar" aria-valuenow="@yield('progress')" aria-valuemin="0" aria-valuemax="100" style="height: 10px">
+            <div class="progress-bar w-@yield('progress')"></div>
+        </div>
+        <div class="quiz1__inner">
+            <h3 class="quiz__question">@yield('question')</h3>
+            @yield('quiz_container')
+            <a href="@yield('next')" class="quiz__link">
+                <div class="quiz__button">Далее</div>
+            </a>
+        </div>
     </div>
 </div>
 @endsection
--- resources/views/quiz1.blade.php
+++ resources/views/quiz1.blade.php
@@ -1,49 +1,44 @@
 @extends('layouts.quiz')
 
+@section('question', 'В какую комнату планируете фотообои?')
+@section('progress', '25')
+@section('next', '/quiz4')
+
 @section('quiz_container')
-    <div class="progress" role="progressbar" aria-label="Progress bar" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100" style="height: 10px">
-        <div class="progress-bar w-25"></div>
-    </div>
-    <div class="quiz1__inner">
-        <h3 class="quiz__question">В какую комнату планируете фотообои?</h3>
-        <div class="container text-center">
-            <div class="row row-cols-4">
-                <div class="col quiz__card">
-                    <img src="{{ Vite::asset('/resources/images/rooms/room1.png') }}" class="rounded mx-auto d-block quiz__card-image" alt="img">
-                    <span class="quiz__caption">Гостиная</span>
-                </div>
-                <div class="col quiz__card">
-                    <img src="{{ Vite::asset('/resources/images/rooms/room2.jpg') }}" class="rounded mx-auto d-block quiz__card-image" alt="img">
-                    <span class="quiz__caption">Спальня</span>
-                </div>
-                <div class="col quiz__card">
-                    <img src="{{ Vite::asset('/resources/images/rooms/room3.jpg') }}" class="rounded mx-auto d-block quiz__card-image" alt="img">
-                    <span class="quiz__caption">Коридор</span>
-                </div>
-                <div class="col quiz__card">
-                    <img src="{{ Vite::asset('/resources/images/rooms/room4.jpg') }}" class="rounded mx-auto d-block quiz__card-image" alt="img">
-                    <span class="quiz__caption">Кухня</span>
-                </div>
-                <div class="col quiz__card">
-                    <img src="{{ Vite::asset('/resources/images/rooms/room5.jpg') }}" class="rounded mx-auto d-block quiz__card-image" alt="img">
-                    <span class="quiz__caption">Детская девочки</span>
-                </div>
-                <div class="col quiz__card">
-                    <img src="{{ Vite::asset('/resources/images/rooms/room6.jpg') }}" class="rounded mx-auto d-block quiz__card-image" alt="img">
-                    <span class="quiz__caption">Детская мальчика</span>
-                </div>
-                <div class="col quiz__card">
-                    <img src="{{ Vite::asset('/resources/images/rooms/room7.jpg') }}" class="rounded mx-auto d-block quiz__card-image" alt="img">
-                    <span class="quiz__caption">Комната подростка</span>
-                </div>
-                <div class="col quiz__card">
-                    <img src="{{ Vite::asset('/resources/images/rooms/room8.png') }}" class="rounded mx-auto d-block quiz__card-image" alt="img">
-                    <span class="quiz__caption">Санузел</span>
-                </div>
-                </div>
+    <div class="cards text-center">
+        <div class="row row-cols-4">
+            <div class="col quiz__card">
+                <img src="{{ Vite::asset('/resources/images/rooms/room1.png') }}" class="rounded mx-auto d-block quiz__card-image" alt="img">
+                <span class="quiz__caption">Гостиная</span>
+            </div>
+            <div class="col quiz__card">
+                <img src="{{ Vite::asset('/resources/images/rooms/room2.jpg') }}" class="rounded mx-auto d-block quiz__card-image" alt="img">
+                <span class="quiz__caption">Спальня</span>
+            </div>
+            <div class="col quiz__card">
+                <img src="{{ Vite::asset('/resources/images/rooms/room3.jpg') }}" class="rounded mx-auto d-block quiz__card-image" alt="img">
+                <span class="quiz__caption">Коридор</span>
+            </div>
+            <div class="col quiz__card">
+                <img src="{{ Vite::asset('/resources/images/rooms/room4.jpg') }}" class="rounded mx-auto d-block quiz__card-image" alt="img">
+                <span class="quiz__caption">Кухня</span>
+            </div>
+            <div class="col quiz__card">
+                <img src="{{ Vite::asset('/resources/images/rooms/room5.jpg') }}" class="rounded mx-auto d-block quiz__card-image" alt="img">
+                <span class="quiz__caption">Детская девочки</span>
+            </div>
+            <div class="col quiz__card">
+                <img src="{{ Vite::asset('/resources/images/rooms/room6.jpg') }}" class="rounded mx-auto d-block quiz__card-image" alt="img">
+                <span class="quiz__caption">Детская мальчика</span>
+            </div>
+            <div class="col quiz__card">
+                <img src="{{ Vite::asset('/resources/images/rooms/room7.jpg') }}" class="rounded mx-auto d-block quiz__card-image" alt="img">
+                <span class="quiz__caption">Комната подростка</span>
+            </div>
+            <div class="col quiz__card">
+                <img src="{{ Vite::asset('/resources/images/rooms/room8.png') }}" class="rounded mx-auto d-block quiz__card-image" alt="img">
+                <span class="quiz__caption">Санузел</span>
+            </div>
         </div>
-        <a href="/quiz4" class="quiz__link">
-            <div class="quiz__button">Далее</div>
-        </a>
     </div>
 @endsection
