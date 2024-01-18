d57ef1746083c97a75847ea43d59c64442f9cf46
Konstantin Davydov <davydovks@mail.ru>
2024-01-18 17:44:14 +0500

    restore csrf_token tags

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,6 +4,8 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <meta name="csrf-param" content="_token">
     <title>Получить визуализацию</title>
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
