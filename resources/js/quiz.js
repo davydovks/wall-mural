ecbab0fb1a6783f66887dbee2408ff1c6f12ffa2
Konstantin Davydov <davydovks@mail.ru>
2024-01-18 13:44:40 +0500

    update quiz1 layout

--- resources/js/quiz.js
+++ resources/js/quiz.js
@@ -1,11 +1,14 @@
-let cards = document.querySelectorAll('.quiz__card-image');
+let images = document.querySelectorAll('.card__image');
 
-cards.forEach(card => {
-    card.addEventListener('click', function() {
-        if (!card.classList.contains("selected")) {
-            cards.forEach((card) => card.classList.remove("selected"));
-            document.querySelector('.quiz__button').style.background == "red";
+images.forEach(image => {
+    image.addEventListener('click', function() {
+        if (!image.parentElement.classList.contains("selected")) {
+            images.forEach((img) => img.parentElement.classList.remove("selected"));
+            document.querySelector('.quiz__button').classList.add("active");
+            image.parentElement.classList.toggle("selected");
+        } else {
+            document.querySelector('.quiz__button').classList.remove("active");
+            image.parentElement.classList.toggle("selected");
         }
-        card.classList.toggle("selected");
     });
 });
--- resources/views/quiz1.blade.php
+++ resources/views/quiz1.blade.php
@@ -4,41 +4,55 @@
 @section('progress', '25')
 @section('next', '/quiz4')
 
+@php
+    $rooms = [
+        [
+            'name' => 'Гостиная',
+            'link' => '/resources/images/rooms/room1.png'
+        ],
+        [
+            'name' => 'Спальня',
+            'link' => '/resources/images/rooms/room2.jpg'
+        ],
+        [
+            'name' => 'Коридор',
+            'link' => '/resources/images/rooms/room3.jpg'
+        ],
+        [
+            'name' => 'Кухня',
+            'link' => '/resources/images/rooms/room4.jpg'
+        ],
+        [
+            'name' => 'Детская девочки',
+            'link' => '/resources/images/rooms/room5.jpg'
+        ],
+        [
+            'name' => 'Детская мальчика',
+            'link' => '/resources/images/rooms/room6.jpg'
+        ],
+        [
+            'name' => 'Комната подростка',
+            'link' => '/resources/images/rooms/room7.jpg'
+        ],
+        [
+            'name' => 'Санузел',
+            'link' => '/resources/images/rooms/room8.jpg'
+        ],
+    ];
+@endphp
+
 @section('quiz_container')
-    <div class="cards text-center">
-        <div class="row row-cols-4">
-            <div class="col quiz__card">
-                <img src="{{ Vite::asset('/resources/images/rooms/room1.png') }}" class="rounded mx-auto d-block quiz__card-image" alt="img">
-                <span class="quiz__caption">Гостиная</span>
+    <div class="cards cards-row-4">
+        @foreach ($rooms as $room)
+            <div class="card">
+                <div class="card__top">
+                    <img src="{{ Vite::asset($room['link']) }}" alt="{{ $room['name'] }}" class="card__image">
+                </div>
+                <div class="card__circle"></div>
+                <div class="card__bottom">
+                    <div class="card__title">{{ $room['name'] }}</div>
+                </div>
             </div>
-            <div class="col quiz__card">
-                <img src="{{ Vite::asset('/resources/images/rooms/room2.jpg') }}" class="rounded mx-auto d-block quiz__card-image" alt="img">
-                <span class="quiz__caption">Спальня</span>
-            </div>
-            <div class="col quiz__card">
-                <img src="{{ Vite::asset('/resources/images/rooms/room3.jpg') }}" class="rounded mx-auto d-block quiz__card-image" alt="img">
-                <span class="quiz__caption">Коридор</span>
-            </div>
-            <div class="col quiz__card">
-                <img src="{{ Vite::asset('/resources/images/rooms/room4.jpg') }}" class="rounded mx-auto d-block quiz__card-image" alt="img">
-                <span class="quiz__caption">Кухня</span>
-            </div>
-            <div class="col quiz__card">
-                <img src="{{ Vite::asset('/resources/images/rooms/room5.jpg') }}" class="rounded mx-auto d-block quiz__card-image" alt="img">
-                <span class="quiz__caption">Детская девочки</span>
-            </div>
-            <div class="col quiz__card">
-                <img src="{{ Vite::asset('/resources/images/rooms/room6.jpg') }}" class="rounded mx-auto d-block quiz__card-image" alt="img">
-                <span class="quiz__caption">Детская мальчика</span>
-            </div>
-            <div class="col quiz__card">
-                <img src="{{ Vite::asset('/resources/images/rooms/room7.jpg') }}" class="rounded mx-auto d-block quiz__card-image" alt="img">
-                <span class="quiz__caption">Комната подростка</span>
-            </div>
-            <div class="col quiz__card">
-                <img src="{{ Vite::asset('/resources/images/rooms/room8.png') }}" class="rounded mx-auto d-block quiz__card-image" alt="img">
-                <span class="quiz__caption">Санузел</span>
-            </div>
-        </div>
+        @endforeach
     </div>
 @endsection
