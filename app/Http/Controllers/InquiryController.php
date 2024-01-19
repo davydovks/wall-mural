967b8b5c9f5cea263130fa0bdd1bc232f9ea020d
Konstantin Davydov <davydovks@mail.ru>
2024-01-19 15:01:14 +0500

    move quiz routes to InquiryController

--- app/Http/Controllers/InquiryController.php
+++ app/Http/Controllers/InquiryController.php
@@ -7,6 +7,43 @@ use Illuminate\Http\Request;
 
 class InquiryController extends Controller
 {
+    public function createStep1()
+    {
+        return view('quiz1');
+    }
+
+    public function storeStep1(Request $request)
+    {
+        $data = $request->validate([
+            'room' => 'required'
+        ]);
+
+        $request->session()->put('room', $data['room']);
+
+        return redirect()->route('inquiries.create4');
+    }
+
+    public function createStep4()
+    {
+        return view('quiz4');
+    }
+
+    public function storeStep4(Request $request)
+    {
+        $data = $request->validate([
+            'position' => 'required'
+        ]);
+
+        $request->session()->put('position', $data['position']);
+
+        return redirect()->route('inquiries.create');
+    }
+
+    public function create()
+    {
+        return view('request');
+    }
+
     public function store(Request $request)
     {
         $data = $this->validate($request, [
@@ -15,6 +52,8 @@ class InquiryController extends Controller
         ]);
 
         $inquiry = new Inquiry($data);
+        $inquiry->room_id = $request->session()->get('room');
+        $inquiry->position_id = $request->session()->get('position');
         $inquiry->save();
 
         return redirect()->route('home');
--- routes/web.php
+++ routes/web.php
@@ -18,17 +18,20 @@ Route::get('/', function () {
     return view('welcome');
 })->name('home');
 
-Route::get('/quiz1', function () {
-    return view('quiz1');
-});
+Route::get('/quiz1', [InquiryController::class, 'createStep1'])
+    ->name('inquiries.create1');
 
-Route::get('/quiz4', function () {
-    return view('quiz4');
-});
+Route::post('/quiz1', [InquiryController::class, 'storeStep1'])
+    ->name('inquiries.store1');
 
-Route::get('/request', function () {
-    return view('request');
-});
+Route::get('/quiz4', [InquiryController::class, 'createStep4'])
+    ->name('inquiries.create4');
+
+Route::post('/quiz4', [InquiryController::class, 'storeStep4'])
+    ->name('inquiries.store4');
+
+Route::get('/request', [InquiryController::class, 'create'])
+    ->name('inquiries.create');
 
 Route::post('/request', [InquiryController::class, 'store'])
     ->name('inquiries.store');
