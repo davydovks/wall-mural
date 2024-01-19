6ac6e0e85daef465fc76d9019b37fec6944924ea
Konstantin Davydov <davydovks@mail.ru>
2024-01-19 14:12:26 +0500

    change selected attribute with checked

--- resources/js/quiz.js
+++ resources/js/quiz.js
@@ -3,12 +3,12 @@ let button = document.querySelector('.quiz__button');
 
 cards.forEach(card => {
     card.addEventListener('click', function () {
-        if (!card.classList.contains("selected")) {
-            cards.forEach((card) => card.classList.remove("selected"));
+        if (!card.classList.contains("checked")) {
+            cards.forEach((card) => card.classList.remove("checked"));
             button.removeAttribute("disabled");
-            card.classList.toggle("selected");
+            card.classList.toggle("checked");
         } else {
-            card.classList.toggle("selected");
+            card.classList.toggle("checked");
             button.setAttribute("disabled", "disabled");
         }
     });
@@ -18,13 +18,13 @@ if (button) {
     button.addEventListener('click', function () {
         if (!button.hasAttribute("disabled")) {
             let url = new URL(window.location.href);
-            let selected = '';
+            let checked = '';
             cards.forEach(function (card, key) {
-                if (card.classList.contains("selected")) {
-                    selected = key;
+                if (card.classList.contains("checked")) {
+                    checked = key;
                 }
             });
-            let cookie = url.pathname + '=' + selected;
+            let cookie = url.pathname + '=' + checked;
             document.cookie = cookie;
         }
     });
