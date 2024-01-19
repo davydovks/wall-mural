100cd925c4c898e546b521561ae09936e7e494d7
Konstantin Davydov <davydovks@mail.ru>
2024-01-19 10:21:21 +0500

    create Upload trait

---
+++ app/Traits/Upload.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Traits;
+
+use Illuminate\Http\UploadedFile;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Str;
+
+
+trait Upload
+{
+    public function uploadFile(UploadedFile $file, $folder = null, $disk = 'public', $filename = null)
+    {
+        $FileName = is_null($filename) ? Str::random(10) : $filename;
+        return $file->storeAs(
+            $folder,
+            $FileName . "." . $file->getClientOriginalExtension(),
+            $disk
+        );
+    }
+
+    public function deleteFile($path, $disk = 'public')
+    {
+        Storage::disk($disk)->delete($path);
+    }
+}
