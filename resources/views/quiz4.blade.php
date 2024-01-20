4c1c067cf2aea30a20ff97adfc4ab884ce399fde
Konstantin Davydov <davydovks@mail.ru>
2024-01-20 20:43:18 +0500

    load positions from db instead of template

--- resources/views/quiz4.blade.php
+++ resources/views/quiz4.blade.php
@@ -5,24 +5,3 @@
 @section('cardsPerRow', '2')
 @section('action', '/quiz4')
 @section('name', 'position')
-
-@php
-    $cards = [
-        [
-            'name' => 'На всю стену',
-            'link' => '/resources/images/positions/position1.jpg'
-        ],
-        [
-            'name' => 'На часть стены',
-            'link' => '/resources/images/positions/position2.jpg'
-        ],
-        [
-            'name' => 'На несколько стен',
-            'link' => '/resources/images/positions/position3.jpg'
-        ],
-        [
-            'name' => 'Пока не определился',
-            'link' => '/resources/images/positions/position4.png'
-        ],
-    ];
-@endphp
